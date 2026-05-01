8558ea6d78f5bd680259be083f6fd8e94ef213f4
Roy <mutuaroymulinge@gmail.com>
2026-05-01 18:54:00 +0300

    fix(products-models): price field change max_length to  max_digits and add default=0 in stock field

--- products/models.py
+++ products/models.py
@@ -15,8 +15,8 @@ class Product(models.Model):
      name = models.CharField(max_length=255)
      slug = models.SlugField(unique=True)
      description = models.TextField()
-     price = models.DecimalField(max_length=100, decimal_places=2) #KES
-     stock = models.PositiveIntegerField()
+     price = models.DecimalField(max_digits=100, decimal_places=2) #KES
+     stock = models.PositiveIntegerField(default=0)
      category = models.ForeignKey(Catergory, on_delete=models.SET_NULL, null=True, blank=True)
      image = models.ImageField(upload_to='products/', blank=True, null=True)
      is_active = models.BooleanField(default=True)
