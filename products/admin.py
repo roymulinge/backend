c2469c0508c821cd840a512cb8dd92c7d73f1766
Roy <mutuaroymulinge@gmail.com>
2026-05-01 21:37:54 +0300

    fix(products):fix syntax plus spelling

--- products/admin.py
+++ products/admin.py
@@ -3,16 +3,16 @@ from .models import Category, Product
 
 @admin.register(Category)
 
-class CategoryAdmin(admin.modelAdmin):
-    prepopulated_fileds = {'slug', ('name', )}
-    lis_display = ('name', 'created_at')
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+    list_display = ('name', 'created_at')
 
 @admin.register(Product)
 
-class ProductAdmin(admin.modelAdmin):
-    prepopulated_fileds = {'slug', ('name',)}
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'price', 'stock', 'category', 'is_active', 'total_value')
     list_filter = ('category', 'is_active', 'created_at')
     search_fields = ('name', 'description')
-    readonly_fields = ('total_value')
+    readonly_fields = ('total_value',)
     
--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Catergory(models.Model):
+class Category(models.Model):
      name = models.CharField(max_length=100, unique=True)
      slug = models.CharField(unique=True)
      created_at = models.DateTimeField(auto_now_add=True)
@@ -17,7 +17,7 @@ class Product(models.Model):
      description = models.TextField()
      price = models.DecimalField(max_digits=100, decimal_places=2) #KES
      stock = models.PositiveIntegerField(default=0)
-     category = models.ForeignKey(Catergory, on_delete=models.SET_NULL, null=True, blank=True)
+     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
      image = models.ImageField(upload_to='products/', blank=True, null=True)
      is_active = models.BooleanField(default=True)
      created_at = models.DateTimeField(auto_now_add=True)
