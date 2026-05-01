6ba95635e5a9fe4f790d96d60b574cb75d32d95c
Roy <mutuaroymulinge@gmail.com>
2026-05-01 13:02:54 +0300

    Replace default user with custom user to user email as username

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique = True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS =[]
+
+    is_shop_owner = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+
