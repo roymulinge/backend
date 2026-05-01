1973c735429fdfd038ad50351995da068161c07c
Roy <mutuaroymulinge@gmail.com>
2026-05-01 19:22:25 +0300

    feat(products-admin):Register Category and product models with custom admin interfaces

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+
+class CategoryAdmin(admin.modelAdmin):
+    prepopulated_fileds = {'slug', ('name', )}
+    lis_display = ('name', 'created_at')
+
+@admin.register(Product)
+
+class ProductAdmin(admin.modelAdmin):
+    prepopulated_fileds = {'slug', ('name',)}
+    list_display = ('name', 'price', 'stock', 'category', 'is_active', 'total_value')
+    list_filter = ('category', 'is_active', 'created_at')
+    search_fields = ('name', 'description')
+    readonly_fields = ('total_value')
+    
