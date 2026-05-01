8b328548dea3b9c334b349258a82b66dd8182bc0
Roy <mutuaroymulinge@gmail.com>
2026-05-01 18:41:25 +0300

    feat(products):add product class and fields and calculate total_value

--- products/models.py
+++ products/models.py
@@ -11,8 +11,26 @@ class Catergory(models.Model):
      def __str__(self):
           return self.name
      
+class Product(models.Model):
+     name = models.CharField(max_length=255)
+     slug = models.SlugField(unique=True)
+     description = models.TextField()
+     price = models.DecimalField(max_length=100, decimal_places=2) #KES
+     stock = models.PositiveIntegerField()
+     category = models.ForeignKey(Catergory, on_delete=models.SET_NULL, null=True, blank=True)
+     image = models.ImageField(upload_to='products/', blank=True, null=True)
+     is_active = models.BooleanField(default=True)
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
 
-
-
+     def __str__(self):
+          return self.name
+     
+     @property
+     def total_value(self):
+          """
+          Total value of stock for this product(price * stock)
+          """
+          return self.price * self.stock
 
 
