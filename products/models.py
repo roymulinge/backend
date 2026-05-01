48421e1cef08ec377d6b1290b3a2b4049b278a7e
Roy <mutuaroymulinge@gmail.com>
2026-05-01 18:27:50 +0300

    feat(products): Define category model and add fields

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Catergory(models.Model):
+     name = models.CharField(max_length=100, unique=True)
+     slug = models.CharField(unique=True)
+     created_at = models.DateTimeField(auto_now_add=True)
+
+     class Meta:
+          verbose_name_plural = 'Categories'
+
+     def __str__(self):
+          return self.name
+     
+
+
+
+
+
